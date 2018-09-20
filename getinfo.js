102d36cc8707bfb7329c525424127bb940a754e4
=khanh chau <viperskye@hotmail.com>
2018-09-20 10:30:03 +0700

    Support array fanpage link

--- getinfo.js
+++ getinfo.js
@@ -20,31 +20,31 @@ graph.setVersion("3.0");
 GetNext = async function(save,res,info = null) {
     let arr = [...save,...res.data];
     if(res.paging && res.paging.next) {
-        console.log('Get next');
+        console.log('>>>Get next');
         let gnext = await graph.getAsync(res.paging.next);
         return await GetNext(arr,gnext,info);
     } else {
-        console.log(`get finish comments of post : ${info}`);
+        console.log(`>>>get finish comments of post : ${info}`);
         return arr;
     }
 }
-GetComments = async function(arrId) {
+GetComments = async function(arrId, fbId, resolve) {
     let arrFull = [];
     for (let i = 0; i < arrId.length; i++) {
         let id = arrId[i].id;
-        console.log(`get comments of post : ${id}`,i);
+        console.log(`>>get comments of post : ${id}`,i);
         let comment = await graph.getAsync(`${id}/comments`, { limit:500,fields:"created_time,from{name,birthday,email,gender,location{name},mobile_phone},message,comment_count", access_token: ACCESS_TOKEN});
         let fullcmt = await GetNext([],comment, id);
         arrFull = [...arrFull,...fullcmt];
-        console.log('next post');
-        console.log(arrFull.length);
+        console.log('>>next post');
     };
     let users = arrFull.map(arr=> {
         return arr.from;
     });
-    WriteComment(users,'hello');
+    console.log(`>Get success comment in page : ${fbId}`);
+    WriteComment(users, fbId, resolve);
 }
-WriteComment = async function(arrRes,info) {
+WriteComment = async function(arrRes, fbId, resolve) {
     let objectUpdate = {};
     arrRes.forEach(element => {
         if(element.location) {
@@ -53,16 +53,13 @@ WriteComment = async function(arrRes,info) {
             objectUpdate[(element.gender || 'None')+'/None/'+element.id] = element;
     })
     await firebase.database().ref('users').update(objectUpdate);
-    console.log('end viewcomments');
+    console.log(`>Save success comment in page ${fbId}`);
+    resolve('success');
 }
-GetPosts = async function(fbId) {
-    console.log('hi');
+GetPosts = async function(fbId, resolve) {
+    console.log(`>Get list posts of page : ${fbId}`);
     let a = await graph.getAsync(`${fbId}/feed`, {limit: NUMBER_POST, fields:"id", access_token: ACCESS_TOKEN});
-    console.log(a);
-	await GetComments(a.data);
-    console.log('finish');
-	console.log('start sendmessages');
-	sendmess();
+	GetComments(a.data, fbId, resolve);
 }
 removeAll = async function() {
     console.log('DELETE ALL DATA ...');
@@ -71,7 +68,22 @@ removeAll = async function() {
     console.log('remove all data successfull !');
 }
 if(REMOVE_OLD === true) removeAll();
-facebook.getId(FANPAGE_LINK, function(id) {
-	if( id == 0 ) id = FANPAGE_LINK;
-    GetPosts(id);
-});
+
+Promise.all(FANPAGE_LINK.map(link => {
+    return new Promise( resolve => {
+        facebook.getId(link, id => {
+            id = id || link;
+            console.log(`Get success ${id}`);
+            resolve(id);
+        })
+    })
+})).then((arrIds) => {
+    return Promise.all(arrIds.map( arrId => {
+        return new Promise( resolve => {
+            GetPosts(arrId,resolve);
+        })
+    }))
+}).then(result => {
+    console.log(result);
+    sendmess();
+})
